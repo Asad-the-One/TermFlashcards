2278985f3dc2e67b377cbd6e99a54e9e61ea9e29
Asad <asadsyedhussain@hotmail.com>
2025-04-27 12:06:52 +0100

    Issue - see file

--- main.py
+++ main.py
@@ -1,32 +1,94 @@
 """
 εεε
+
+Issues:
+. File not saving properly after practice
 """
 
 import tkinter as tk
 from random import randint
+import os
+
+# State globals
+words = []
+term = ""
+point = ""
 
 # Get list of words
-words = []
-f = open("words.txt", "r")
-for i in f:
-    words.append(i.strip().split("εεε"))
-f.close()
+def getWords(name):
+    global words
+    f = open(name, "r")
+    for i in f:
+        words.append(i.strip().split("εεε"))
+    f.close()
+
+# Update file
+def updateFile(name):
+    f = open(name, "w")
+    for i in words:
+        f.write(f"{i[0]}εεε{i[1]}εεε{i[2]}\n")
+    f.close()
 
 # Create window
 win = tk.Tk()
 win.geometry("500x200")
 
-# State globals
-term = ""
-point = ""
+# Frame, menu
 
-# Main frame
+def menu():
 
-def main():
-    
     # Create frame
     f1 = tk.Frame(win, padx=5, pady=5)
     f1.pack()
+
+    ############## Labels
+
+    # Header
+    l1 = tk.Label(f1, text="Filler Title")
+    l1.pack()
+
+    ############## Dropdown with packs
+
+    # Get list of ".txt" files
+    files = []
+    for i in os.listdir():
+        if i[-4:] == ".txt":
+            files.append(i)
+    
+    # Stringvar with first value in list
+    v = tk.StringVar(f1, files[0])
+
+    # Create dropdown menu
+    o1 = tk.OptionMenu(f1, v, *files)
+    o1.pack()
+
+    ############## Buttons
+
+    # Button to enter flashcards
+    def startFlash():
+        global file
+        f1.destroy()
+        file = v.get()
+        getWords(file)
+        flashes()
+    b1 = tk.Button(f1, text="Practice", command=startFlash)
+    b1.pack()
+
+    # Button to quit
+    def bye():
+        win.destroy()
+    b3 = tk.Button(f1, text="Quit", command=bye)
+    b3.pack()
+
+# Frame with flashcards
+
+def flashes():
+    
+    # Create frame
+    f2 = tk.Frame(win, padx=5, pady=5)
+    f2.pack()
+
+    ############## Labels
 
     # Select random word, and confirm selection based on score
     def getTerm():
@@ -43,11 +105,11 @@ def main():
     getTerm()
 
     # Create label on top that has the term
-    l1 = tk.Label(f1, text=term[0])
+    l1 = tk.Label(f2, text=term[0])
     l1.pack()
 
     # Create label that displays score
-    l2 = tk.Label(f1,text=term[2])
+    l2 = tk.Label(f2,text=term[2])
     l2.pack()
 
     ############## Buttons
@@ -58,7 +120,7 @@ def main():
             l1.config(text=term[1])
         else:
             l1.config(text=term[0])
-    b1 = tk.Button(f1, text="Switch", command=switch)
+    b1 = tk.Button(f2, text="Switch", command=switch)
     b1.pack()
 
     # Create button to input "I know this". Changes word
@@ -68,7 +130,7 @@ def main():
         getTerm()
         l1.config(text=term[0])
         l2.config(text=term[2])
-    b2 = tk.Button(f1, text="Correct", command=correct)
+    b2 = tk.Button(f2, text="Correct", command=correct)
     b2.pack()
 
     # Create button to input "I don't know this". Changes word
@@ -79,18 +141,55 @@ def main():
         getTerm()
         l1.config(text=term[0])
         l2.config(text=term[2])
-    b3 = tk.Button(f1, text="Wrong", command=wrong)
+    b3 = tk.Button(f2, text="Wrong", command=wrong)
     b3.pack()
 
     # Create button to quit
     def byebye():
-        win.destroy()
-    b4 = tk.Button(f1, text="Quit", command=byebye)
+        f2.destroy()
+        updateFile(file)
+        menu()
+    b4 = tk.Button(f2, text="Quit", command=byebye)
     b4.pack()
 
-main()
+# Create a new pack
+def newPack():
+    pass
+
+
+# Summon menu and loop
+menu()
 win.mainloop()
 
-f = open("words.txt", "w")
-for i in words:
-    f.write(f"{i[0]}εεε{i[1]}εεε{i[2]}\n")
+"""
+# Importing Tkinter module
+from tkinter import *
+# from tkinter.ttk import *
+ 
+# Creating master Tkinter window
+master = Tk()
+master.geometry("175x175")
+ 
+# Tkinter string variable
+# able to store any string value
+v = StringVar(master, "1")
+ 
+# Dictionary to create multiple buttons
+values = {"RadioButton 1" : "1",
+          "RadioButton 2" : "2",
+          "RadioButton 3" : "3",
+          "RadioButton 4" : "4",
+          "RadioButton 5" : "5"}
+ 
+# Loop is used to create multiple Radiobuttons
+# rather than creating each button separately
+for (text, value) in values.items():
+    Radiobutton(master, text = text, variable = v, 
+                value = value, indicator = 0,
+                background = "light blue").pack(fill = X, ipady = 5)
+ 
+# Infinite loop can be terminated by
+# keyboard or mouse interrupt
+# or by any predefined function (destroy())
+mainloop()
+"""
